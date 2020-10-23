0037817dd4d69c545a19e3bd358da36ae82c1e0d
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2020-10-23 12:28:58 -0300

    Improvements

--- postbuild.sh
+++ postbuild.sh
@@ -29,5 +29,7 @@ echo "Create application folder: $folder_name"
 mkdir -p $folder_name
 cp -rf build/* $folder_name
 
-find $folder_name"/index.html" -type f | xargs sed -i 's/\/static\/js/{{ STATIC_URL }}'$folder_name'\/static\/js/g'
-find $folder_name"/index.html" -type f | xargs sed -i 's/\/static\/css/{{ STATIC_URL }}'$folder_name'\/static\/css/g'
+find $folder_name"/index.html" -type f | xargs sed -i "s/\/static\/js/{{ STATIC_URL }}$folder_name\/static\/js/g"
+find $folder_name"/index.html" -type f | xargs sed -i "s/\/static\/css/{{ STATIC_URL }}$folder_name\/static\/css/g"
+find $folder_name"/index.html" -type f | xargs sed -i "s/images\/favicons/{{ STATIC_URL }}$folder_name\/images\/favicons/g"
+find $folder_name"/index.html" -type f | xargs sed -i "s/http:\/\/localhost:3000\/images/http:\/\/obahia.dea.ufv.br\/static\/$folder_name\/images/g"
